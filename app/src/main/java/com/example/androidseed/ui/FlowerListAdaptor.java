5a265ed869106ebd96f6d0ed28a95ed73c520d6b
Asanga Manage <asanga.manage@auspost.com.au>
2016-07-07 23:49:43 +1000

    Add Picasso library

--- app/src/main/java/com/example/androidseed/ui/FlowerListAdaptor.java
+++ app/src/main/java/com/example/androidseed/ui/FlowerListAdaptor.java
@@ -5,15 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.androidseed.R;
 import com.example.androidseed.models.Flower;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by asangamanage on 19/05/2016.
  */
 public class FlowerListAdaptor extends ArrayAdapter<Flower> {
+
+    final String IMAGE_HOST_URL = "http://services.hanselandpetal.com/photos/";
+
     public FlowerListAdaptor(Context context, int resource) {
         super(context, resource);
     }
@@ -26,8 +31,12 @@ public class FlowerListAdaptor extends ArrayAdapter<Flower> {
         }
         TextView txtFlowerName = (TextView) convertView.findViewById(R.id.txtFlowerName);
         TextView txtFlowerDescription = (TextView) convertView.findViewById(R.id.txtFlowerDescription);
+        ImageView imgFlower = (ImageView) convertView.findViewById(R.id.imgFlower);
+        String url = IMAGE_HOST_URL + flower.photo;
+        Picasso.with(getContext()).load(url).placeholder(android.R.drawable.ic_notification_overlay).into(imgFlower);
         txtFlowerName.setText(flower.name);
         txtFlowerDescription.setText(flower.category);
         return convertView;
     }
+
 }
